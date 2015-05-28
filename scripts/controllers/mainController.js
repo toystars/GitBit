a9a5bd009caa49f33ba69a169625ac3be35c66a9
andela-bmustapha <mustapha.babtunde@andela.co>
2015-05-28 13:15:17 +0100

    Created user object models, repo Array model, following and followers Array model

--- scripts/controllers/mainController.js
+++ scripts/controllers/mainController.js
@@ -4,11 +4,76 @@ app.controller('mainController', ['$scope', '$mdSidenav', 'apiCall', function($s
     $mdSidenav(menuId).toggle();
   };
 
-  // function that consumes the apiCall service to fetch the data from github
-  $scope.fectUser = function(username) {
-    apiCall.fetchUser(username).success(function(data) {
-      $scope.data = data;
-    });
+  // app models
+  $scope.searchQuery = '';  // model for search query
+
+  // function that assigns data to meodels from the api call
+  function manipulateApiResponse(data) {
+    $scope.data = data; // assign the object returned to a scope variable
+    
+    // assign to models
+    $scope.userFullName = $scope.data['name'];
+    $scope.userImageUrl = $scope.data['avatar_url'];
+    $scope.repoUrl = 'https://github.com/' + $scope.searchQuery + '?tab=repositories';
+    $scope.userProfileLink = 'https://github.com/' + $scope.searchQuery;
+    $scope.numberOfPublicRepos = $scope.data['public_repos'];
+    $scope.userLogin = $scope.data['login'];
+    $scope.userEmail = $scope.data['email'];
+    $scope.userLocation = $scope.data['location'];
+    $scope.userAvailableForHire = $scope.data['hireable'];
+    $scope.userCompany = $scope.data['company'];
   };
 
+  // function that assigns data to followers models
+  function manipulateFollowersArray(data) {
+    $scope.followersArray = data;
+  }
+
+  // function that assigns data to following models
+  function manipulateFollowingArray(data) {
+    $scope.followingArray = data;
+  }
+
+  // function that assigns data to repo models
+  function manipulateRepoArray(data) {
+    $scope.repoArray = data;
+  }
+
+  // function that consumes the apiCall service to fetch the data from github
+  $scope.getUserObject = function(username) {
+    apiCall.fetchUser(username).success(manipulateApiResponse);
+  };
+
+  $scope.getFollowersArray = function(username) {
+    apiCall.fetchFollowers(username).success(manipulateFollowersArray);
+  };
+
+  $scope.getFollowingArray = function(username) {
+    apiCall.fetchFollowing(username).success(manipulateFollowingArray);
+  };
+
+  $scope.getRepoArray = function(username) {
+    apiCall.fetchRepo(username).success(manipulateRepoArray);
+  }
+
+
 }]);
+
+
+
+/*
+// app models
+  $scope.searchQuery = '';
+  $scope.userFullName = '';
+  $scope.userImageUrl = '';
+  $scope.repoUrl = '';
+  $scope.userProfileLink = '';
+  $scope.followersApiUrl = '';
+  $scope.followingApiUrl = '';
+  $scope.numberOfPublicRepos = 0;
+  $scope.userLogin = '';
+  $scope.userEmail = '';
+  $scope.userLocation = '';
+  $scope.userAvailableForHire = false;
+  $scope.userCompany = '';
+*/
--- scripts/services/api-call.js
+++ scripts/services/api-call.js
@@ -4,9 +4,20 @@
 */
 
 app.factory('apiCall',['$http', function($http) {
+
+  var apiAuth = '?client_id=c20d4cc3d83f7d505737&client_secret=39777a8b06af0c4ee6090d8ec7c6e4a6a326c0e1';
   return {
     fetchUser: function(username) {
-      return  $http.get('https://api.github.com/search/users?q=' + username);
+      return  $http.get('https://api.github.com/users/' + username + apiAuth);
+    },
+    fetchFollowers: function(username) {
+      return  $http.get('https://api.github.com/users/' + username + '/followers' + apiAuth);
+    },
+    fetchFollowing: function(username) {
+      return  $http.get('https://api.github.com/users/' + username + '/following' + apiAuth);
+    },
+    fetchRepo: function(username) {
+      return  $http.get('https://api.github.com/users/' + username + '/repos' + apiAuth);
     }
   };
 }]);
